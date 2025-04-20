3c71c60e21bcc21860ab9ac87ea1c1eea07f830b
Kdus17 <kidus2460@gmail.com>
2025-04-20 21:46:45 +0300

    added order model

---
+++ models/OrderModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+  {
+    customer_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    products: {
+      type: [[mongoose.Schema.Types.ObjectId, Number]],
+      required: true,
+    },
+    total_price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const OrderModel = mongoose.model("order", OrderSchema);
+export default OrderModel;

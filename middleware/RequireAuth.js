eb71126a217e9ade8cea1ff86b5404475db38c7f
Kdus17 <kidus2460@gmail.com>
2025-05-03 10:58:01 +0300

    added the middleware for the token

---
+++ middleware/RequireAuth.js
@@ -0,0 +1,17 @@
+import {verifyToken} from "../utils/ValidateToken.js";
+
+export const requireAuth = (req, res, next) => {
+    const {authorization} = req.headers;
+
+    if (!authorization) {
+        return res.status(401).json({error: 'Authorization required'});
+    }
+    const token = authorization.split(' ')[1];
+    try {
+        const payload = verifyToken(token);
+        next()
+    }
+    catch(err){
+        return res.status(401).json({error:"Request is not authorized"})
+    }
+}

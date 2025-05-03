08a4746c671104359f20ad73de5223dc2b35d689
Kdus17 <kidus2460@gmail.com>
2025-05-03 11:05:15 +0300

    updated the server

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 import express from "express";
-import { connect_db } from "./config/db";
+import cors from "cors";
+import { connectToDB } from "./config/db.js";
+import product_router from "./routers/ProductRouters.js";
+import order_router from "./routers/OrderRouters.js";
+import user_router from "./routers/UserRouters.js";
 
 const app = express();
-connect_db().then(() => {
-  app.listen(8000, () => {
-    console.log("asd");
+const corsOptions = {
+  origin: ["https://autoshoopa.vercel.app"], // Or an array of allowed origins
+  methods: ["GET", "PUT", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // If you need to handle cookies
+  maxAge: 3600,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use("/api/products", product_router);
+app.use("/api/orders", order_router);
+app.use("/api/auth", user_router);
+
+connectToDB().then(() => {
+  app.listen(8080, () => {
+    console.log("Server running on port 8080");
   });
 });
